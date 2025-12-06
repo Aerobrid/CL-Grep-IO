e8415dc45aaf0f3464a7572cfd09f76d066de9dc
Raj Patel <raj721patel@gmail.com>
2025-12-05 23:14:17 -0600

    semicolon fix + Reading Files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // explicit type annotation here since .collect() can return many types of collections
-    // and Rust compiler can't infer which one you want
+    // and Rust compiler can't infer (implicit) which one you want
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
     let file_path = &args[2];
 
     println!("Searching for {query}");
-    println!("In file {file_path}")
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
