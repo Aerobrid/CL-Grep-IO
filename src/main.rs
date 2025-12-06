e891fdb8dcc650a4f2c69ef110acdffa84b627d7
Raj Patel <raj721patel@gmail.com>
2025-12-06 15:36:40 -0600

    Refactoring to Improve Modularity and Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,55 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+use minigrep::search;
 
 fn main() {
     // explicit type annotation here since .collect() can return many types of collections
     // and Rust compiler can't infer (implicit) which one you want
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    // || {...} syntax -> closure -> anonymous functions in Rust
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // in case of an error return said error back to caller (main) with ?
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        print!("{line}");
+    }  
 
     println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    // in case of Err Rust compiler needs to know lifetime of str slice (static for entire duration of program)
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        // we create deep copy with .clone() to avoid managing lifetime references/issues (negligible tradeoff)
+        // if you want overkill you could annotate lifetimes 
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
